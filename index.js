fa3ed2392e5fd0c2c02962c13cadcd1075577ace
Abdikhafar <abdikhafarissack@gmail.com>
2024-07-07 15:34:59 +0300

    dom edit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element
+let newHeader = document.createElement('h1');
+
+// Assign an id of 'victory' to the new <h1> element
+newHeader.id = 'victory';
+
+// Set the inner text of the new <h1> element
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+// Append the new <h1> element to the body or another element in the DOM
+document.body.appendChild(newHeader);
